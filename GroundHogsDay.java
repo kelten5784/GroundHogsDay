b8ef21774177d501d88a22e177929e0f59694df3
kelten5784 <49505489+kelten5784@users.noreply.github.com>
2020-11-06 08:58:29 -0500

    Update GroundHogsDay.java

--- GroundHogsDay.java
+++ GroundHogsDay.java
@@ -1,17 +1,17 @@
-	// Import the GUI libraries
-	import javax.swing.*;
+// Import the GUI libraries
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-
 import java.util.Scanner;
 import javax.swing.Timer;
 import java.awt.*;
-	import java.awt.event.*;
+
+import java.awt.event.*;
 public class GroundHogsDay {
 
 	/**
 	 * MAIN METHOD
 	 * This main method starts the GUI and runs the createMainWindow() method.
-     * This method should not be changed.
+	 * This method should not be changed.
 	 */
 	public static void main (String [] args) {
 		javax.swing.SwingUtilities.invokeLater (new Runnable () {
@@ -25,32 +25,32 @@ public class GroundHogsDay {
 	/**
 	 * STATIC VARIABLES AND CONSTANTS
 	 * Declare the objects and variables that you want to access across
-     * multiple methods.
+	 * multiple methods.
 	 */
 
 
 
-	 // CREATE MAIN WINDOW
-     // This method is called by the main method to set up the main GUI window.
-	
-	 static JLabel mapImage = new JLabel(new ImageIcon("grassbackground.png"));
-		
+	// CREATE MAIN WINDOW
+	// This method is called by the main method to set up the main GUI window.
+
+	static JLabel mapImage = new JLabel(new ImageIcon("grassbackground.png"));
+
 	private static void createMainWindow () {
 		// Create and set up the window.
 		JFrame frame = new JFrame ("Ground Hogs Day");
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		frame.setResizable (false);
-		
+
 		// The panel that will hold the components in the frame.
 		JLayeredPane contentPane = new JLayeredPane ();
 		contentPane.setPreferredSize(new Dimension (700, 394));
-		
+
 		//add the map.
 		mapImage.setSize(700, 394);
 		contentPane.add(mapImage);
-	
 
-		
+
+
 		// right pane 
 		JPanel sideBar = new JPanel();
 		sideBar.setLayout(new BoxLayout(sideBar, BoxLayout.PAGE_AXIS));
@@ -59,34 +59,45 @@ public class GroundHogsDay {
 		contentPane.add(sideBar);
 		contentPane.setLayer(sideBar, 100);
 		sideBar.setLocation(525, 0);
-		
+
 		//Components to right panel
 		int scorePoints = 0;
 		JLabel Points = new JLabel("Points " + scorePoints );
 		Points.setFont(new Font("Comic Sans MS", Font.ITALIC | Font.BOLD, 16));
 		sideBar.add(Points);
-		
+
 		//timer
-		  int delay = 100; //milliseconds
-		  ActionListener taskPerformer = new ActionListener() {
-			  //a loser frame if the time limit runs out
-		      public void actionPerformed(ActionEvent evt) {
-		    	  JFrame loser = new JFrame ("Loser");
-		  		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		  		frame.setResizable (false);
-		  		
-		  		//buttons and labels 
-		  		JLabel gameOver = new JLabel("Game Over");
-		  		gameOver.setFont(new Font("Comic Sans MS", Font.ITALIC | Font.BOLD, 25));
-		          
-		      }
-		  };
-		  new Timer(delay, taskPerformer).start();
+		int delay = 30000; //milliseconds
+		ActionListener taskPerformer = new ActionListener() {
+
+			JFrame loser = new JFrame ();
+
+			//a loser frame if the time limit runs out
+			public void actionPerformed(ActionEvent evt) {
+				if (delay == 30000) {  
+					loser = new JFrame ();
+					loser.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+					loser.setResizable (false);
+					loser.setLocationRelativeTo(null);
+					loser.setVisible(true);
+					frame.setVisible(false);
+					//buttons and labels 
+					JLabel gameOver = new JLabel("Game Over");
+					gameOver.setFont(new Font("Comic Sans MS", Font.ITALIC | Font.BOLD, 25));
+					loser.add(gameOver);
+					loser.setPreferredSize(new Dimension (400 , 200));
+				}
+			}
+		};
+
+		new Timer(delay, taskPerformer).start();
+
+		//JLabel timer = new JLabel(delay);
 
 
-	
 
-        //Add components to the content pane panel
+
+		//Add components to the content pane panel
 
 
 		// Add the panel to the frame
@@ -96,20 +107,23 @@ public class GroundHogsDay {
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+
+
+
+
+		/**
+		 * HELPER METHODS
+		 * Methods that you create to manage repetitive tasks.
+		 */
+
+
+
+		/**
+		 * EVENT LISTENERS
+		 * Subclasses that handle events (button clicks, mouse clicks and moves,
+		 * key presses, timer expirations)
+		 */
+
 	}
-
-
-    /**
-     * HELPER METHODS
-     * Methods that you create to manage repetitive tasks.
-     */
-
-
-
-    /**
-     * EVENT LISTENERS
-     * Subclasses that handle events (button clicks, mouse clicks and moves,
-     * key presses, timer expirations)
-     */
-
 }
+
